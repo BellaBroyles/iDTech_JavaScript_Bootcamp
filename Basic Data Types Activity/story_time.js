bc291158157320b3c595b23c9ab0153f6bab4f8a
BellaBroyles <86171930+BellaBroyles@users.noreply.github.com>
2022-03-06 13:18:58 -0500

    Add files via upload

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,39 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE(theme: Death Note)
+let intro = "Mankind is evil. With the power I had discovered I could've save the world.";
+let myName = "Light Yagami";
+let title = "Kira";
+let type = "God of the New World."
+
+
+
+// TODO: Create 3-String(//integers you mean?) variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 23;
+let year = 2010;
+let peopleKilled = 124925;
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let namesinBook = ['L Lawliet','Raye Penber','Naomi Misora','Kiyomi Takada'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let killedInnocents = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(intro + " My name is " + myName + ".");
+console.log("I am " + title);
+console.log("I was the " + type + " This is my farewell address.");
+console.log("When Near suceeded in his efforts to defeat me I was " + age + " years old.");
+console.log("It was the year " + year + ".");
+console.log("Through all my years as " + title + ", I wrote " + peopleKilled + " names in my Death Note.");
+console.log("I was cleansing this world of criminals. I saved everyone.")
+console.log("In order to continue my just crusade, I had to kill those who wished to stop me.");
+console.log("Me: So...you killed innocent people? To what--protect yourself?")
+console.log("Light: Don't look at me like that. But " + killedInnocents + " I did.")
+console.log(namesinBook[1] + " and " + namesinBook[2] + " were two.")
+console.log("This concludes my confession. I--Light Yagami, am Kira. The God of The New World.")
+console.log("I hope all is well without me.")
+console.log("                                                                     -Light ")
+
